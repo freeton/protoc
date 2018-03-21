834da7d753d6e58c5b7629c045971b00e49e0d89
CM-20180307ZWYV\Administrator <zh_freeton@msn.com>
2018-03-21 17:53:23 +0800

    创建

---
+++ src/main/java/com/cmcm/oa/protoc/Start.java
@@ -0,0 +1,48 @@
+package com.cmcm.oa.protoc;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 说明：
+ *
+ * @author zhangwei
+ * @date 2018/3/21 11:13
+ */
+public class Start {
+
+    public static void main(String[] args) throws IOException {
+        PersonMsg.Person.Builder builder=PersonMsg.Person.newBuilder();
+        builder.setId(1);
+        builder.setEmail("123@cmcm.com");
+        builder.setName("haha");
+
+        PersonMsg.Person person=builder.build();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        person.writeTo(output);
+
+
+        byte[] byteArray = output.toByteArray();
+
+        // -------------- 分割线：下面是接收方，将数据接收后反序列化 ---------------
+
+        // 接收到流并读取，如网络输入流，这里用ByteArrayInputStream来代替
+        ByteArrayInputStream input = new ByteArrayInputStream(byteArray);
+
+        // 反序列化
+        PersonMsg.Person xxg2 = PersonMsg.Person.parseFrom(input);
+        System.out.println("ID:" + xxg2.getId());
+        System.out.println("name:" + xxg2.getName());
+        System.out.println("email:" + xxg2.getEmail());
+        System.out.println("friend:");
+        List<String> friends = xxg2.getFriendsList();
+        for(String friend : friends) {
+            System.out.println(friend);
+        }
+
+
+    }
+
+}
